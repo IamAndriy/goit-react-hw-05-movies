3043b5bffbbb291c47ce9f2ed8e9a672065aca06
Olga Dzubak <>
2025-02-08 03:05:32 +0200

    update

--- src/hooks/useFetchMovieReviews.js
+++ src/hooks/useFetchMovieReviews.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import { getMovieReviews } from "api/api";
+import { getMovieReviews } from "../api/api";
 
 export const useFetchMovieReviews = () => {
 
