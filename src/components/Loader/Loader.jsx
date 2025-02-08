5a810eb8981ce80afeb93c23a13274cb1b3695b5
Olga Dzubak <>
2025-02-08 21:04:07 +0200

    Update Loader.jsx

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,4 +1,14 @@
+import {Discuss} from "react-loader-spinner";
+
 export const Loader = () => {
-    return  <div>
-            </div>
+    return  <Discuss
+                visible={true}
+                height="80"
+                width="80"
+                ariaLabel="discuss-loading"
+                wrapperStyle={{}}
+                wrapperClass="discuss-wrapper"
+                color="#fff"
+                backgroundColor="#F4442E"
+            />
 }
